e70dacb26dbc4b0302377add408a9a7114181ffc
wangyang <wangyang2013@ict.ac.cn>
2014-10-16 17:03:50 +0800

    123

---
+++ sfa/sfa_config.sh
@@ -0,0 +1,44 @@
+# DO NOT EDIT. This file was automatically generated at
+# Fri Jul 25 11:17:51 2014 from:
+# 
+# /etc/sfa/sfa_config
+# /etc/sfa/default_config.xml
+# /etc/sfa/configs/site_config
+SFA_GENERIC_FLAVOUR="pl"
+SFA_INTERFACE_HRN="ceni"
+SFA_CREDENTIAL_SCHEMA="/etc/sfa/credential.xsd"
+SFA_API_LOGLEVEL=2
+SFA_MAX_SLICE_RENEW=60
+SFA_SESSION_KEY_PATH="/var/lib/sfa/session_keys"
+SFA_DATA_DIR="/var/lib/sfa/"
+SFA_REGISTRY_ENABLED=True
+SFA_REGISTRY_HOST="localhost"
+SFA_REGISTRY_PORT=12345
+SFA_REGISTRY_ROOT_AUTH="ceni"
+SFA_SM_ENABLED=True
+SFA_SM_HOST="localhost"
+SFA_SM_PORT=12347
+SFA_SM_CACHING=False
+SFA_AGGREGATE_ENABLED=True
+SFA_AGGREGATE_HOST="localhost"
+SFA_AGGREGATE_PORT=12346
+SFA_AGGREGATE_CACHING=True
+SFA_DB_ENABLED=True
+SFA_DB_HOST="localhost"
+SFA_DB_PORT=5432
+SFA_DB_USER="sfadbuser"
+SFA_DB_PASSWORD="6bfcd2c9-0236-405a-89ab-5938603970c9"
+SFA_DB_NAME="sfa"
+SFA_FLASHPOLICY_ENABLED=False
+SFA_FLASHPOLICY_CONFIG_FILE="/etc/sfa/sfa_flashpolicy_config.xml"
+SFA_FLASHPOLICY_PORT=843
+SFA_PLC_USER="wangyang2013@ict.ac.cn"
+SFA_PLC_PASSWORD="myplcmyslice"
+SFA_PLC_URL="https://localhost:443/PLCAPI/"
+SFA_FEDERICA_URL="https://root:password@federica.sfa.wrapper.com:8443/fedewrapper/xmlrpc/"
+SFA_NOVA_USER="novaadmin"
+SFA_NOVA_API_URL="127.0.0.1"
+SFA_NOVA_API_PORT=8773
+SFA_NOVA_NOVARC="/root/novarc"
+SFA_NITOS_URL="http://195.251.17.239:8080/RPC2"
+SFA_DUMMY_URL="http://127.0.0.1:8080"
